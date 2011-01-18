a26a6ce1a8a00dfad16b8fcc794113eff639fba9
gsavin <guilhelm.savin@graphstream-project.org>
2011-01-18 18:50:35 +0100

    Update licence preamble

--- src/org/graphstream/algorithm/generator/package-info.java
+++ src/org/graphstream/algorithm/generator/package-info.java
@@ -1,54 +1,31 @@
 /*
- * This file is part of GraphStream.
+ * Copyright 2006 - 2011 
+ *     Julien Baudry	<julien.baudry@graphstream-project.org>
+ *     Antoine Dutot	<antoine.dutot@graphstream-project.org>
+ *     Yoann Pigné		<yoann.pigne@graphstream-project.org>
+ *     Guilhelm Savin	<guilhelm.savin@graphstream-project.org>
  * 
- * GraphStream is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
+ * This file is part of GraphStream <http://graphstream-project.org>.
  * 
- * GraphStream is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
+ * GraphStream is a library whose purpose is to handle static or dynamic
+ * graph, create them from scratch, file or any source and display them.
  * 
- * You should have received a copy of the GNU General Public License
- * along with GraphStream.  If not, see <http://www.gnu.org/licenses/>.
+ * This program is free software distributed under the terms of two licenses, the
+ * CeCILL-C license that fits European law, and the GNU Lesser General Public
+ * License. You can  use, modify and/ or redistribute the software under the terms
+ * of the CeCILL-C license as circulated by CEA, CNRS and INRIA at the following
+ * URL <http://www.cecill.info> or under the terms of the GNU LGPL as published by
+ * the Free Software Foundation, either version 3 of the License, or (at your
+ * option) any later version.
  * 
- * Copyright 2006 - 2010
- * 	Julien Baudry
- * 	Antoine Dutot
- * 	Yoann Pigné
- * 	Guilhelm Savin
- */
-
-/**
- * Various graph generators with specific properties.
+ * This program is distributed in the hope that it will be useful, but WITHOUT ANY
+ * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A
+ * PARTICULAR PURPOSE.  See the GNU Lesser General Public License for more details.
  * 
- * <p>
- * Graph generators are classes that can be used as iterators, but instead of
- * iterating on already existing elements, they create them on the fly. Often
- * the more one iterates on the generator, the larger the graph becomes. Some
- * generator are finite, whereas others are not.
- * </p>
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  * 
- * <p>
- * Generators have very few methods (apart from the generation parameter setup
- * methods that are specific to each generator). They are:
- * <ul>
- * 		<li>{@link org.graphstream.algorithm.generator.Generator#begin()}
- * 			to start the generation of elements in the given graph. This graph
- * 			may or not already contain elements.</li>
- * 		<li>{@link org.graphstream.algorithm.generator.Generator#nextEvents()}
- * 			to generate one or a set of elements in the graph. The more this
- * 			method is iterated, the more the graph grows. This method returns
- * 			false when no more elements can be generated. BE CAREFUL: some
- * 			generators never return false: they can generate graphs of arbitrary
- * 			size.</li>
- * 		<li>{@link org.graphstream.algorithm.generator.Generator#end()} to
- * 			finish the graph generation process, either once
- * 			{@link org.graphstream.algorithm.generator.Generator#nextEvents()}
- * 			returned false, or one chooses to stop the generation process.</li>
- * </ul>
- * </p>
+ * The fact that you are presently reading this means that you have had
+ * knowledge of the CeCILL-C and LGPL licenses and that you accept their terms.
  */
 package org.graphstream.algorithm.generator;
