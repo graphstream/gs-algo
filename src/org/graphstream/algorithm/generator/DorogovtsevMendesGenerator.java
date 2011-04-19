8455d821acc1fc17bc1cb5dc75689e034043f819
gsavin <guilhelm.savin@graphstream-project.org>
2011-04-19 14:55:53 +0200

    Add ref to Dorogovtsev -- Mendes generator.

--- src/org/graphstream/algorithm/generator/DorogovtsevMendesGenerator.java
+++ src/org/graphstream/algorithm/generator/DorogovtsevMendesGenerator.java
@@ -62,6 +62,9 @@ import java.util.Random;
  * therefore generate trees of any size.
  * </p>
  * 
+ * S. N. Dorogovtsev and J. F. F. Mendes, "Evolution of networks", in Adv. Phys,
+ * 2002, 1079--1187
+ * 
  * @since 20070117
  */
 public class DorogovtsevMendesGenerator extends BaseGenerator {
