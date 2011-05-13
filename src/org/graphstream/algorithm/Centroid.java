aec856c2741dfe1cb1d035837da2ea0077f69330
gsavin <guilhelm.savin@graphstream-project.org>
2011-05-13 13:40:18 +0200

    Document Centroid and Eccentricity algorithms.

--- src/org/graphstream/algorithm/Centroid.java
+++ src/org/graphstream/algorithm/Centroid.java
@@ -36,44 +36,171 @@ import org.graphstream.graph.Graph;
 import org.graphstream.graph.Node;
 
 /**
- * Compute the centroid of a connected graph. In a graph G, if d(u,v) is the
- * shortest length between two nodes u and v (ie the number of edges of the
- * shortest path) let m(u) be the sum of d(u,v) for all nodes v of G. Centroid
- * of a graph G is a subgraph induced by vertices u with minimum m(u). (Centers
- * and Centroids of unicyclic graph, Miroslav Truszczynski, 1985)
+ * Compute the centroid of a connected graph.
  * 
+ * <p>
+ * In a graph G, if d(u,v) is the shortest length between two nodes u and v (ie
+ * the number of edges of the shortest path) let m(u) be the sum of d(u,v) for
+ * all nodes v of G. Centroid of a graph G is a subgraph induced by vertices u
+ * with minimum m(u).
+ * </p>
+ * 
+ * <h2>Requirements</h2>
+ * 
+ * <p>
  * This algorithm needs that APSP algorithm has been computed before its own
  * computation.
+ * </p>
+ * 
+ * <h2>Example</h2>
+ * 
+ * <pre>
+ * import java.io.StringReader;
+ * import java.io.IOException;
+ * 
+ * import org.graphstream.algorithm.Centroid;
+ * import org.graphstream.graph.Graph;
+ * import org.graphstream.graph.implementations.DefaultGraph;
+ * import org.graphstream.stream.file.FileSourceDGS;
+ * 
+ * //                     +--- E
+ * // A --- B --- C -- D -|--- F
+ * //                     +--- G
+ * 
+ * public class CentroidTest {
+ * 	static String my_graph = &quot;DGS004\n&quot; + &quot;my 0 0\n&quot; + &quot;an A \n&quot; + &quot;an B \n&quot;
+ * 			+ &quot;an C \n&quot; + &quot;an D \n&quot; + &quot;an E \n&quot; + &quot;an F \n&quot; + &quot;an G \n&quot;
+ * 			+ &quot;ae AB A B \n&quot; + &quot;ae BC B C \n&quot; + &quot;ae CD C D \n&quot; + &quot;ae DE D E \n&quot;
+ * 			+ &quot;ae DF D F \n&quot; + &quot;ae DG D G \n&quot;;
+ * 
+ * 	public static void main(String[] args) throws IOException {
+ * 		Graph graph = new DefaultGraph(&quot;Centroid Test&quot;);
+ * 		StringReader reader = new StringReader(my_graph);
+ * 
+ * 		FileSourceDGS source = new FileSourceDGS();
+ * 		source.addSink(graph);
+ * 		source.readAll(reader);
+ * 
+ * 		APSP apsp = new APSP();
+ * 		apsp.init(graph);
+ * 		apsp.compute();
+ * 
+ * 		Centroid centroid = new Centroid();
+ * 		centroid.init(graph);
+ * 		centroid.compute();
+ * 
+ * 		for (Node n : graph.getEachNode()) {
+ * 			Boolean in = n.getAttribute(&quot;centroid&quot;);
+ * 
+ * 			System.out.printf(&quot;%s is%s in the centroid.\n&quot;, n.getId(), in ? &quot;&quot;
+ * 					: &quot; not&quot;);
+ * 		}
+ * 
+ * 		// Output will be :
+ * 		//
+ * 		// A is not in the centroid
+ * 		// B is not in the centroid
+ * 		// C is not in the centroid
+ * 		// D is in the centroid
+ * 		// E is not in the centroid
+ * 		// F is not in the centroid
+ * 		// G is not in the centroid
+ * 	}
+ * }
+ * </pre>
  * 
  * @complexity O(n2)
+ * @see org.graphstream.algorithm.APSPAlgorithm.APSPInfo
  * @reference F. Harary, Graph Theory. Westview Press, Oct. 1969. [Online].
  *            Available: http://www.amazon.com/exec/obidos/
  *            redirect?tag=citeulike07-20\&path=ASIN/ 0201410338
  */
 public class Centroid implements Algorithm {
-
 	/**
 	 * The graph on which centroid is computed.
 	 */
 	protected Graph graph;
+
 	/**
 	 * Attribute in which APSPInfo are stored.
 	 */
-	protected String apspInfoAttribute = APSP.APSPInfo.ATTRIBUTE_NAME;
+	protected String apspInfoAttribute;
+
 	/**
 	 * Attribute to store centroid information.
 	 */
-	protected String centroidAttribute = "centroid";
+	protected String centroidAttribute;
+
 	/**
 	 * Value of the attribute if node is in the centroid.
 	 */
-	protected Object isInCentroid = Boolean.TRUE;
+	protected Object isInCentroid;
+
 	/**
 	 * Value of the attribute if node is not in the centroid.
 	 */
-	protected Object isNotInCentroid = Boolean.FALSE;
+	protected Object isNotInCentroid;
 
+	/**
+	 * Build a new centroid algorithm with default parameters.
+	 */
 	public Centroid() {
+		this("centroid");
+	}
+
+	/**
+	 * Build a new centroid algorithm, specifying the attribute name of the
+	 * computation result
+	 * 
+	 * @param centroidAttribute
+	 *            attribute name of the computation result.
+	 */
+	public Centroid(String centroidAttribute) {
+		this(centroidAttribute, Boolean.TRUE, Boolean.FALSE);
+	}
+
+	/**
+	 * Build a new centroid as in {@link #Centroid(String)} but specifying
+	 * values of centroid membership.
+	 * 
+	 * @param centroidAttribute
+	 *            attribute name of the computation result.
+	 * @param isInCentroid
+	 *            the value of elements centroid attribute when this element is
+	 *            in the centroid.
+	 * @param isNotInCentroid
+	 *            the value of elements centroid attribute when this element is
+	 *            not in the centroid.
+	 */
+	public Centroid(String centroidAttribute, Object isInCentroid,
+			Object isNotInCentroid) {
+		this(centroidAttribute, Boolean.TRUE, Boolean.FALSE,
+				APSP.APSPInfo.ATTRIBUTE_NAME);
+	}
+
+	/**
+	 * Build a new centroid algorithm as in
+	 * {@link #Centroid(String, Object, Object)} but specifying the name of the
+	 * attribute where the APSP informations are stored.
+	 * 
+	 * @param centroidAttribute
+	 *            attribute name of the computation result.
+	 * @param isInCentroid
+	 *            the value of elements centroid attribute when this element is
+	 *            in the centroid.
+	 * @param isNotInCentroid
+	 *            the value of elements centroid attribute when this element is
+	 *            not in the centroid.
+	 * @param apspInfoAttribute
+	 *            the name of the attribute where the APSP informations are
+	 *            stored
+	 */
+	public Centroid(String centroidAttribute, Object isInCentroid,
+			Object isNotInCentroid, String apspInfoAttribute) {
+		this.centroidAttribute = centroidAttribute;
+		this.isInCentroid = isInCentroid;
+		this.isNotInCentroid = isNotInCentroid;
+		this.apspInfoAttribute = apspInfoAttribute;
 	}
 
 	/*
@@ -130,5 +257,94 @@ public class Centroid implements Algorithm {
 					centroid.contains(node) ? isInCentroid : isNotInCentroid);
 
 		centroid.clear();
+	}
+
+	/**
+	 * Get the APSP info attribute name.
+	 * 
+	 * @return the name of the attribute where the APSP informations are stored.
+	 */
+	public String getAPSPInfoAttribute() {
+		return apspInfoAttribute;
+	}
+
+	/**
+	 * Set the APSP info attribute name.
+	 * 
+	 * @param attribute
+	 *            the name of the attribute where the APSP informations are
+	 *            stored.
+	 */
+	public void setAPSPInfoAttribute(String attribute) {
+		apspInfoAttribute = attribute;
+	}
+
+	/**
+	 * Get the value of the centroid attribute when element is in the centroid.
+	 * Default value is Boolean.TRUE.
+	 * 
+	 * @return the value of elements centroid attribute when this element is in
+	 *         the centroid.
+	 */
+	public Object getIsInCentroidValue() {
+		return isInCentroid;
+	}
+
+	/**
+	 * Set the value of the centroid attribute when element is in the centroid.
+	 * On computation, this value is used to set the centroid attribute.
+	 * 
+	 * @param value
+	 *            the value of elements centroid attribute when this element is
+	 *            in the centroid.
+	 */
+	public void setIsInCentroidValue(Object value) {
+		isInCentroid = value;
+	}
+
+	/**
+	 * Get the value of the centroid attribute when element is not in the
+	 * centroid. Default value is Boolean.FALSE.
+	 * 
+	 * @return the value of elements centroid attribute when this element is not
+	 *         in the centroid.
+	 */
+	public Object getIsNotInCentroidValue() {
+		return isNotInCentroid;
+	}
+
+	/**
+	 * Set the value of the centroid attribute when element is not in the
+	 * centroid. On computation, this value is used to set the centroid
+	 * attribute.
+	 * 
+	 * @param value
+	 *            the value of elements centroid attribute when this element is
+	 *            not in the centroid.
+	 */
+	public void setIsNotInCentroidValue(Object value) {
+		isNotInCentroid = value;
+	}
+
+	/**
+	 * Get the name of the attribute where computation result is stored. Value
+	 * of this attribute can be {@link #getIsInCentroidValue()} if the element
+	 * is in the centroid, {@link #getIsNotInCentroidValue()} else.
+	 * 
+	 * @return the centroid attribute name.
+	 */
+	public String getCentroidAttribute() {
+		return centroidAttribute;
+	}
+
+	/**
+	 * Set the name of the attribute where computation result is stored.
+	 * 
+	 * @param centroidAttribute
+	 *            the name of the element attribute where computation result is
+	 *            stored.
+	 */
+	public void setCentroidAttribute(String centroidAttribute) {
+		this.centroidAttribute = centroidAttribute;
 	}
 }
--- src/org/graphstream/algorithm/Eccentricity.java
+++ src/org/graphstream/algorithm/Eccentricity.java
@@ -36,16 +36,84 @@ import org.graphstream.graph.Graph;
 import org.graphstream.graph.Node;
 
 /**
- * Compute the eccentricity of a connected graph. In a graph G, if d(u,v) is the
- * shortest length between two nodes u and v (ie the number of edges of the
- * shortest path) let e(u) be the d(u,v) such that v is the farthest of u.
- * Eccentricity of a graph G is a subgraph induced by vertices u with minimum
- * e(u). (Centers and Centroids of unicyclic graph, Miroslav Truszczynski, 1985)
+ * Compute the eccentricity of a connected graph.
  * 
+ * <p>
+ * In a graph G, if d(u,v) is the shortest length between two nodes u and v (ie
+ * the number of edges of the shortest path) let e(u) be the d(u,v) such that v
+ * is the farthest of u. Eccentricity of a graph G is a subgraph induced by
+ * vertices u with minimum e(u).
+ * </p>
+ * 
+ * <h2>Requirements</h2>
+ *
+ * <p>
  * This algorithm needs that APSP algorithm has been computed before its own
  * computation.
+ * </p>
+ * 
+ * <h2>Example</h2>
+ * 
+ * <pre>
+ * import java.io.StringReader;
+ * import java.io.IOException;
+ * 
+ * import org.graphstream.algorithm.Centroid;
+ * import org.graphstream.graph.Graph;
+ * import org.graphstream.graph.implementations.DefaultGraph;
+ * import org.graphstream.stream.file.FileSourceDGS;
+ * 
+ * //                     +--- E
+ * // A --- B --- C -- D -|--- F
+ * //                     +--- G
+ * 
+ * public class EccentriciyTest {
+ * 	static String my_graph = &quot;DGS004\n&quot; + &quot;my 0 0\n&quot; + &quot;an A \n&quot; + &quot;an B \n&quot;
+ * 			+ &quot;an C \n&quot; + &quot;an D \n&quot; + &quot;an E \n&quot; + &quot;an F \n&quot; + &quot;an G \n&quot;
+ * 			+ &quot;ae AB A B \n&quot; + &quot;ae BC B C \n&quot; + &quot;ae CD C D \n&quot; + &quot;ae DE D E \n&quot;
+ * 			+ &quot;ae DF D F \n&quot; + &quot;ae DG D G \n&quot;;
+ * 
+ * 	public static void main(String[] args) throws IOException {
+ * 		Graph graph = new DefaultGraph(&quot;Centroid Test&quot;);
+ * 		StringReader reader = new StringReader(my_graph);
+ * 
+ * 		FileSourceDGS source = new FileSourceDGS();
+ * 		source.addSink(graph);
+ * 		source.readAll(reader);
+ * 
+ * 		APSP apsp = new APSP();
+ * 		apsp.init(graph);
+ * 		apsp.compute();
+ * 
+ * 		Eccentricity eccentricity = new Eccentricity();
+ * 		eccentricity.init(graph);
+ * 		eccentricity.compute();
+ * 
+ * 		for (Node n : graph.getEachNode()) {
+ * 			Boolean in = n.getAttribute(&quot;eccentricity&quot;);
+ * 
+ * 			System.out.printf(&quot;%s is%s in the eccentricity.\n&quot;, n.getId(), in ? &quot;&quot;
+ * 					: &quot; not&quot;);
+ * 		}
+ * 
+ * 		// Output will be :
+ * 		//
+ * 		// A is not in the eccentricity
+ * 		// B is not in the eccentricity
+ * 		// C is in the eccentricity
+ * 		// D is not in the eccentricity
+ * 		// E is not in the eccentricity
+ * 		// F is not in the eccentricity
+ * 		// G is not in the eccentricity
+ * 	}
+ * }
+ * </pre>
  * 
  * @complexity O(n2)
+ * @see org.graphstream.algorithm.APSPAlgorithm.APSPInfo
+ * @reference F. Harary, Graph Theory. Westview Press, Oct. 1969. [Online].
+ *            Available: http://www.amazon.com/exec/obidos/
+ *            redirect?tag=citeulike07-20\&path=ASIN/0201410338
  */
 public class Eccentricity implements Algorithm {
 
@@ -70,7 +138,66 @@ public class Eccentricity implements Algorithm {
 	 */
 	protected Object isNotInEccentricity = Boolean.FALSE;
 
+	/**
+	 * Build a new eccentricity algorithm with default parameters.
+	 */
 	public Eccentricity() {
+		this("eccentricity");
+	}
+
+	/**
+	 * Build a new eccentricity algorithm, specifying the attribute name of the
+	 * computation result
+	 * 
+	 * @param eccentricityAttribute
+	 *            attribute name of the computation result.
+	 */
+	public Eccentricity(String eccentricityAttribute) {
+		this(eccentricityAttribute, Boolean.TRUE, Boolean.FALSE);
+	}
+
+	/**
+	 * Build a new eccentricity as in {@link #Eccentricity(String)} but
+	 * specifying values of eccentricity membership.
+	 * 
+	 * @param eccentricityAttribute
+	 *            attribute name of the computation result.
+	 * @param isInEccentricity
+	 *            the value of elements eccentricity attribute when this element
+	 *            is in the eccentricity.
+	 * @param isNotInEccentricity
+	 *            the value of elements eccentricity attribute when this element
+	 *            is not in the eccentricity.
+	 */
+	public Eccentricity(String eccentricityAttribute, Object isInEccentricity,
+			Object isNotInEccentricity) {
+		this(eccentricityAttribute, Boolean.TRUE, Boolean.FALSE,
+				APSP.APSPInfo.ATTRIBUTE_NAME);
+	}
+
+	/**
+	 * Build a new eccentricity algorithm as in
+	 * {@link #Eccentricity(String, Object, Object)} but specifying the name of
+	 * the attribute where the APSP informations are stored.
+	 * 
+	 * @param eccentricityAttribute
+	 *            attribute name of the computation result.
+	 * @param isInEccentricity
+	 *            the value of elements eccentricity attribute when this element
+	 *            is in the eccentricity.
+	 * @param isNotInEccentricity
+	 *            the value of elements eccentricity attribute when this element
+	 *            is not in the eccentricity.
+	 * @param apspInfoAttribute
+	 *            the name of the attribute where the APSP informations are
+	 *            stored
+	 */
+	public Eccentricity(String eccentricityAttribute, Object isInEccentricity,
+			Object isNotInEccentricity, String apspInfoAttribute) {
+		this.eccentricityAttribute = eccentricityAttribute;
+		this.isInEccentricity = isInEccentricity;
+		this.isNotInEccentricity = isNotInEccentricity;
+		this.apspInfoAttribute = apspInfoAttribute;
 	}
 
 	/*
@@ -106,7 +233,7 @@ public class Eccentricity implements Algorithm {
 
 					if (d < 0)
 						System.err
-								.printf("Found a negative length value in centroid algorithm. "
+								.printf("Found a negative length value in eccentricity algorithm. "
 										+ "Is graph connected ?\n");
 					else if (d > m)
 						m = d;
@@ -125,7 +252,98 @@ public class Eccentricity implements Algorithm {
 		for (Node node : graph.getEachNode())
 			node.setAttribute(eccentricityAttribute, eccentricity
 					.contains(node) ? isInEccentricity : isNotInEccentricity);
-		
+
 		eccentricity.clear();
+	}
+
+	/**
+	 * Get the APSP info attribute name.
+	 * 
+	 * @return the name of the attribute where the APSP informations are stored.
+	 */
+	public String getAPSPInfoAttribute() {
+		return apspInfoAttribute;
+	}
+
+	/**
+	 * Set the APSP info attribute name.
+	 * 
+	 * @param attribute
+	 *            the name of the attribute where the APSP informations are
+	 *            stored.
+	 */
+	public void setAPSPInfoAttribute(String attribute) {
+		apspInfoAttribute = attribute;
+	}
+
+	/**
+	 * Get the value of the eccentricity attribute when element is in the
+	 * eccentricity. Default value is Boolean.TRUE.
+	 * 
+	 * @return the value of elements eccentricity attribute when this element is
+	 *         in the eccentricity.
+	 */
+	public Object getIsInEccentricityValue() {
+		return isInEccentricity;
+	}
+
+	/**
+	 * Set the value of the eccentricity attribute when element is in the
+	 * eccentricity. On computation, this value is used to set the eccentricity
+	 * attribute.
+	 * 
+	 * @param value
+	 *            the value of elements eccentricity attribute when this element
+	 *            is in the eccentricity.
+	 */
+	public void setIsInEccentricityValue(Object value) {
+		isInEccentricity = value;
+	}
+
+	/**
+	 * Get the value of the eccentricity attribute when element is not in the
+	 * eccentricity. Default value is Boolean.FALSE.
+	 * 
+	 * @return the value of elements eccentricity attribute when this element is
+	 *         not in the eccentricity.
+	 */
+	public Object getIsNotInEccentricityValue() {
+		return isNotInEccentricity;
+	}
+
+	/**
+	 * Set the value of the eccentricity attribute when element is not in the
+	 * eccentricity. On computation, this value is used to set the eccentricity
+	 * attribute.
+	 * 
+	 * @param value
+	 *            the value of elements eccentricity attribute when this element
+	 *            is not in the eccentricity.
+	 */
+	public void setIsNotInEccentricityValue(Object value) {
+		isNotInEccentricity = value;
+	}
+
+	/**
+	 * Get the name of the attribute where computation result is stored. Value
+	 * of this attribute can be {@link #getIsInEccentricityValue()} if the
+	 * element is in the eccentricity, {@link #getIsNotInEccentricityValue()}
+	 * else.
+	 * 
+	 * @return the eccentricity attribute name.
+	 */
+	public String getEccentricityAttribute() {
+		return eccentricityAttribute;
+	}
+
+	/**
+	 * Set the name of the attribute where computation result is stored.
+	 * 
+	 * @param eccentricityAttribute
+	 *            the name of the element attribute where computation result is
+	 *            stored.
+	 */
+	public void setEccentricityAttribute(String eccentricityAttribute) {
+		this.eccentricityAttribute = eccentricityAttribute;
 	}
 }
